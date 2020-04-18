03c7f3858dc756b6c04322f82dcaa3b2a2dcdc5b
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-04-18 16:10:58 +0300

    Update vowels7.py

--- Part_3_dict_set_tuple/vowels7.py
+++ Part_3_dict_set_tuple/vowels7.py
@@ -4,11 +4,14 @@
 vowels = ['a', 'e', 'i', 'u', 'o']
 # word = 'Milliways'
 word = input('Provide a word to search for vowels: ')
-found = []
+# found = []
 
-for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found.append(letter)
+# for letter in word:
+#     if letter in vowels:
+#         if letter not in found:
+#             found.append(letter)
+
+found = list(set(word).intersection(set(vowels)))
+
 for vowel in found:
     print(vowel)
