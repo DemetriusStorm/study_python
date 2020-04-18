3033edcb74e8d4740720b2b59589fc94774c3503
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-04-18 20:15:07 +0300

    Update vsearch.py

--- part_4_function/vsearch.py
+++ part_4_function/vsearch.py
@@ -7,8 +7,7 @@ def search4vowels():
     vowels = set('aeiou')
     word = input('Provide a word to search for vowels: ')
     found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    return ''.join(list(found))
 
 
-search4vowels()
+print(search4vowels())
