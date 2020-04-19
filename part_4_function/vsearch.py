1e8d933f01e8096bc562d8a7e9dd56191a6d8af2
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-04-19 19:17:11 +0300

    Update vsearch.py

--- part_4_function/vsearch.py
+++ part_4_function/vsearch.py
@@ -17,5 +17,31 @@ def search_for_vowels(word: str) -> str:
     return ''.join(list(found))
 
 
-# print(search4vowels())
-print(search_for_vowels('sdfsfaikdsohfa'))
+def search_for_vowels2(word: str) -> bool:
+    """ Return bool if any vowels found in a supplied word.
+        Function name in PEP8"""
+    vowels = set('aeiou')
+    found = vowels.intersection(set(word))
+    return bool(found)
+
+
+def search_for_vowels3(word: str) -> set:
+    """ Return finded vowels in word"""
+    vowels = set('aeiou')
+    return vowels.intersection(set(word))
+
+
+def search_letters_in_phrase(phrase: str, letters: str) -> set:
+    """ Return letters in phrase"""
+    return set(letters).intersection(set(phrase))
+
+
+def search_letters_in_phrase2(phrase: str, letters: str = 'aeiou') -> set:
+    """ Return letters in phrase."""
+    return set(letters).intersection(set(phrase))
+
+
+print(help(search_letters_in_phrase2))
+print(search_letters_in_phrase2('dskjf$hskaTgfey235', 'sdfsfaWTG#$%'))
+print(search_letters_in_phrase2('dskjf$hskaTgfey235', letters='sdfsfaWTG#$%'))
+print(search_letters_in_phrase2(letters='sdfsfaWTG#$%', phrase='dskjf$hskaTgfey235'))
