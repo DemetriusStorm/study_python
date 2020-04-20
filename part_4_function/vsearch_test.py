bb06ca4db9f1416984cfcc4db4504d05ceb8ec3b
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-04-20 14:26:34 +0300

    Update vsearch_test.py

--- part_4_function/vsearch_test.py
+++ part_4_function/vsearch_test.py
@@ -1,3 +1,30 @@
+# def search4vowels():
+#     vowels = set('aeiou')
+#     word = input('Provide a word to search for vowels: ')
+#     found = vowels.intersection(set(word))
+#     return ''.join(list(found))
+
+
+# def search_for_vowels(word: str) -> str:
+#     """Print vowels, finded in input word."""
+#     vowels = set('aeiou')
+#     found = vowels.intersection(set(word))
+#     return ''.join(list(found))
+
+
+# def search_for_vowels2(word: str) -> bool:
+#     """ Return bool if any vowels found in a supplied word.
+#         Function name in PEP8"""
+#     vowels = set('aeiou')
+#     found = vowels.intersection(set(word))
+#     return bool(found)
+
+
+# def search_letters_in_phrase(phrase: str, letters: str) -> set:
+#     """ Return letters in phrase"""
+#     return set(letters).intersection(set(phrase))
+
+
 def search4vowels():
     """
     Print and position vowels in set from input text.
