546907d63d59698f35ca2e64307a335d8a21b148
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-04-20 15:24:42 +0300

    Create setup.py

---
+++ part_4_function/mymodules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The Head First Python Search Tools',
+    author='Dmitriy Bulygin',
+    author_email='demetrius.storm@gmail.com',
+    url='https://github.com/DemetriusStorm',
+    py_modules=['vsearch'],
+)
