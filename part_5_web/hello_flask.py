c6e811c622cc795f5f84b0c10a8d3f7ba6cbb431
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2020-04-24 21:34:36 +0300

    Create hello_flask.py

---
+++ part_5_web/hello_flask.py
@@ -0,0 +1,22 @@
+"""
+Introduce with flask!
+"""
+
+from flask import Flask
+from _public.study_python.part_4_function.mymodules.vsearch import search_letters_in_phrase as search4letters
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello() -> str:
+    return 'Hello world from Flask!'
+
+
+@app.route('/search4')
+def do_search() -> str:
+    print('In phrase: Life, the universe, and everything!\n')
+    return str(search4letters('Life, the universe, and everything!'))
+
+
+app.run()
